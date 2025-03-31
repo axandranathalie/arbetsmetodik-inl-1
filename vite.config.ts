aa19a5df61a43c5545ec601afc0a30bc255557f8
Alexandra <al3xandra92@hotmail.com>
2025-03-31 13:50:23 +0200

    Update vite config for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/fed24d-arbetsmetodik-inl-1-axandranathalie/',
+  base: '/arbetsmetodik-inl-1/', // Byt till ditt nya repo-namn här
   build: {
     cssCodeSplit: true, // Gör att CSS delas upp om du har flera CSS-filer
     minify: 'esbuild', // Minifierar CSS och JS med esbuild
